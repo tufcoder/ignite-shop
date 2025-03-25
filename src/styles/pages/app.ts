b13a9a96511473faff4edf246586aae7e71c8b96
Oswaldo Castro <oswaldogpc@gmail.com>
2025-03-25 19:44:39 -0300

    Cabeçalho da aplicação

---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+export const Header = styled('header', {
+  maxWidth: 1100,
+  width: '100%',
+  margin: '0 auto',
+  padding: '2rem 0',
+})
