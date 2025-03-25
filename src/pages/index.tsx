05056249a6cc68e77367ffc0aa243682de2fbcda
Oswaldo Castro <oswaldogpc@gmail.com>
2025-03-25 19:25:44 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  border: 0,
+  borderRadius: 4,
+  padding: '4px 8px',
+  backgroundColor: '$rocketseat',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+})
+
 export default function Home() {
   return (
     <div>
-      <h1>Next.js App</h1>
+      <Button>
+        <span>Teste</span>
+        Enviarfff
+      </Button>
     </div>
   )
 }
