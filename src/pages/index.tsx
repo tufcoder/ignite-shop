9f5bd4e49d22ec71d2bf3fc4e71f35ba51aeda44
Oswaldo Castro <oswaldogpc@gmail.com>
2025-03-26 18:37:35 -0300

    Integração com o Stripe: Utilizando SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
@@ -47,7 +47,7 @@ export default function Home({ products }:HomeProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async () => {
+export const getStaticProps:GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -66,6 +66,7 @@ export const getServerSideProps:GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
